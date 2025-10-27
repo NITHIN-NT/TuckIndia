1501792c7d2ce2a0819a15985de902b48cb59035
Nithin <nithinraj07sachu@gmail.com>
2025-10-27 17:07:19 +0530

    Check Point Backup - 1

--- TuckProject/settings.py
+++ TuckProject/settings.py
@@ -2,6 +2,10 @@ from pathlib import Path
 import os
 import environ
 
+#============================checkpoint========================
+    # BackUp - 1 Homepage / breadcrums / Product listing / Product Category
+#============================checkpoint========================
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
